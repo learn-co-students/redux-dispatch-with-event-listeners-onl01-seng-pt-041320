5f4b603c6c69b5347aea0e47bc53d934de5c48d5
jahman07104 <jahman@bellatlantic.net>
2021-01-21 13:44:09 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,39 @@
 // add code snippets from README
+// let state = {count: 0}
+// the event listender works at the top too but I moved it to the bottom
+
+// let button = document.getElementById('button');
+ 
+// button.addEventListener('click', () => {
+//   dispatch({type: 'INCREASE_COUNT'})
+// })
+
+let state;
+ 
+function reducer(state = {count: 0}, action){
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1}
+    default:
+      return state;
+  }
+}
+ 
+function dispatch(action){
+  state = reducer(state, action)
+  render()
+}
+ 
+function render(){
+  let container = document.getElementById('container');
+  container.textContent = state.count;
+}
+
+
+let button = document.getElementById('button');
+ 
+button.addEventListener('click', () => {
+  dispatch({type: 'INCREASE_COUNT'})
+})
+ 
+dispatch({type: '@@INIT'})
